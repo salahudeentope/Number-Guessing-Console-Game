a2d09c907ef0beed844f80a8ff7b4f6e86ef6fed
Tope Salahudeen <salahudeentope@gmail.com>
2020-04-07 22:31:55 +0100

    added comments to the code.

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,13 +1,20 @@
-import random
+import random  # will be used to generate the random number
 
 
 def right_answer():
+    """
+    A function to generate a random number between 1 and the maximum permitted number (n)
+    based on the level selected by the player.
+
+    :return: the right answer (answer), number of guesses allowed (guesses)
+     and the maximum number (n)
+    """
     print('Easy: guess a number between 1-10 with 6 guesses')
     print('Medium: Guess a number between 1-20 with 4 guesses')
     print('Hard: Guess a number between 1-50 with 3 guesses \n')
     level = input("Kindly input the level you want to play [easy, medium, hard]: ")
 
-    while level.lower() not in ['easy', 'medium', 'hard']:
+    while level.lower() not in ['easy', 'medium', 'hard']:  # to check that no invalid selection was made.
         level = input("Kindly input a valid level selection [easy, medium, hard]: ")
     if level.lower() == 'easy':
         n = 10
@@ -24,12 +31,21 @@ def right_answer():
 
 
 def guessing_game():
+    """
+    A function to take the players guess as an input and compare it to the right answer generated from the
+    right_answer() function.
+
+    :return: Prints that is wrong is the player inputs a number different from the right_answer,
+    and prints "you got it right!" if the guess is same as the answer. I also prints "game over!" if
+    the player runs out of guesses.
+    """
     answer, guesses, n = right_answer()
 
-    while guesses > 0:
+    while guesses > 0:  # to check if the player still has guesses left.
         print(f"you have {guesses} guesses left")
         while True:
-            try:
+            try:  # to check if the player inputs something different from a number or a number out of range for the
+                # level the player is playing.
                 user_guess = int(input(f'Kindly input a guess between 1 - {n}: '))
             except ValueError:
                 print(f'That is not a valid guess')
@@ -40,10 +56,10 @@ def guessing_game():
                 else:
                     break
 
-        if user_guess != answer:
+        if user_guess != answer:  # if the player's guess is wrong.
             print("That was wrong!")
             guesses -= 1
-            if guesses == 0:
+            if guesses == 0:  # When the player is out of guesses.
                 print('Game Over!')
             continue
         else:
